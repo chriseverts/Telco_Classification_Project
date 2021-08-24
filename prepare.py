7ab19aa68aaad76b9b7cd5e2aabe4eda94bd83c5
chriseverts <chriseverts21@gmail.com>
2021-08-24 14:22:04 -0500

    added code

--- prepare.py
+++ prepare.py
@@ -4,86 +4,143 @@
 # In[ ]:
 
 
-from pydataset import data
-import seaborn as sns
 import pandas as pd
 import numpy as np
 import os
-from env import host, username, password, get_db_url
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
 
-def prep_iris(df):
-    '''
-    takes in a dataframe of the iris dataset as it is acquired and returns a cleaned dataframe
-    arguments: df: a pandas DataFrame with the expected feature names and columns
-    return: clean_df: a dataframe with the cleaning operations performed on it
-    '''
-    df = df.drop(columns=['species_id', 'measurement_id'])
-    df = df.rename(columns={'species_name': 'species'})
-    dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=[True])
-    df = pd.concat([df, dummy_df], axis=1)
-    return df
+from env import host, username, password
+from acquire import get_connection, new_telco_churn_data, get_telco_churn_data
 
-def prep_titanic(df):
-    '''
-    This function take in the titanic data acquired by get_titanic_data,
-    Returns prepped train, validate, and test dfs with embarked dummy vars,
-    deck dropped, and the mean of age imputed for Null values.
-    '''
-    
-    # drop rows where embarked/embark town are null values
-    df = df[~df.embarked.isnull()]
-    
-    # encode embarked using dummy columns
-    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
-    
-    # join dummy columns back to df
-    df = pd.concat([df, titanic_dummies], axis=1)
-    
-    # drop the deck column
-    df = df.drop(columns='deck')
-    
-    # split data into train, validate, test dfs
-    train, validate, test = titanic_split(df)
-    
-    # impute mean of age into null values in age column
-    train, validate, test = impute_mean_age(train, validate, test)
-    
-    return train, validate, test
+from sklearn.model_selection import train_test_split
 
-def titanic_split(df):
+#Prep telco_churn
+
+def num_distributions(df):
     '''
-    This function take in the titanic data acquired by get_titanic_data,
-    performs a split and stratifies survived column.
-    Returns train, validate, and test dfs.
+    This functions takes in a dataframe and displays
+    the distribution of each numeric column.
     '''
+    for col in df.columns:
+        if df[col].dtype != 'object':
+            plt.hist(df[col])
+            plt.title(f'Distribution of {col}')
+            plt.show()
+
+def clean_telco_churn(df):
+    '''cleans our telco churn data for us, and makes dummies'''
+    #df.replace('', np.nan, regex = True)
+    #is.na.sum()
+    #total_charges had 11 null values. I changed them into a float, and filled nulls with the mean of all total charges.
+    df.total_charges = pd.to_numeric(df.total_charges, errors='coerce').astype('float64')
+    df.total_charges = df.total_charges.fillna(value=df.total_charges.mean()).astype('float64')
+    df.replace('No internet service', 'No', inplace=True)
+    df.replace('No phone service', 'No', inplace = True)
+
+    #making dummies for columns I found appropriate to do so. I just went down the list. 
+    df["is_female"] = df.gender == "Female"
+    df['is_female'] = (df['is_female']).astype(int)
+
+    df["partner"] = df.partner == "Yes"
+    df['partner'] = (df['partner']).astype(int)
+
+    df["dependents"] = df.dependents == "Yes"
+    df['dependents'] = (df['dependents']).astype(int)
+
+    df["phone_service"] = df.phone_service == "Yes"
+    df['phone_service'] = (df['phone_service']).astype(int)
+
+    df["streaming_tv"] = df.streaming_tv == "Yes"
+    df['streaming_tv'] = (df['streaming_tv']).astype(int)
+
+    df["streaming_movies"] = df.streaming_movies == "Yes"
+    df['streaming_movies'] = (df['streaming_movies']).astype(int)
+
+    df["paperless_billing"] = df.paperless_billing == "Yes"
+    df['paperless_billing'] = (df['paperless_billing']).astype(int)
+
+    df["churn"] = df.churn == "Yes"
+    df['churn'] = (df['churn']).astype(int)
+
+    df["multiple_lines"] = df.multiple_lines == "Yes"
+    df['multiple_lines'] = (df['multiple_lines']).astype(int)
+
+    df["online_security"] = df.online_security == "Yes"
+    df['online_security'] = (df['online_security']).astype(int)
+
+    df["online_backup"] = df.online_backup == "Yes"
+    df['online_backup'] = (df['online_backup']).astype(int)
+
+    df["device_protection"] = df.device_protection == "Yes"
+    df['device_protection'] = (df['device_protection']).astype(int)
+
+    df["tech_support"] = df.tech_support == "Yes"
+    df['tech_support'] = (df['tech_support']).astype(int)
+
+    #dropping redundant columns
+    df = df.drop(columns =['payment_type_id', 'contract_type_id', 'internet_service_type_id'])
+    df = df.drop(columns=['gender'])
+    #making a dummy df, and combining it back to the original df. Dropping redundant columns again.
+    dummy_df = pd.get_dummies(df[['internet_service_type', 'contract_type','payment_type']], drop_first=False)
+    dummy_df = dummy_df.rename(columns={'internet_service_type_DSL': 'dsl',
+                                   'internet_service_type_Fiber optic': 'fiber_optic',
+                                   'internet_service_type_None': 'no_internet',
+                                   'contract_type_Month-to-month': 'monthly',
+                                   'contract_type_One year': 'one_year',
+                                   'contract_type_Two year': 'two_year',
+                                   'payment_type_Bank transfer (automatic)': 'bank_transfer',
+                                   'payment_type_Credit card (automatic)': 'credit_card',
+                                   'payment_type_Electronic check': 'electronic_check',
+                                   'payment_type_Mailed check': 'mailed_check'})
+    df = pd.concat([df, dummy_df], axis =1)
+    df = df.drop(columns=['internet_service_type','contract_type','payment_type'])
+
+    return df 
+
+
+def telco_churn_split(df):
+    #splitting our data
     train_validate, test = train_test_split(df, test_size=.2, 
                                         random_state=123, 
-                                        stratify=df.survived)
+                                        stratify=df.churn)
     train, validate = train_test_split(train_validate, test_size=.3, 
                                    random_state=123, 
-                                   stratify=train_validate.survived)
+                                   stratify=train_validate.churn)
     return train, validate, test
 
 
-def impute_mean_age(train, validate, test):
-    '''
-    This function imputes the mean of the age column for
-    observations with missing values.
-    Returns transformed train, validate, and test df.
-    '''
-    # create the imputer object with mean strategy
-    imputer = SimpleImputer(strategy = 'mean')
-    
-    # fit on and transform age column in train
-    train['age'] = imputer.fit_transform(train[['age']])
-    
-    # transform age column in validate
-    validate['age'] = imputer.transform(validate[['age']])
-    
-    # transform age column in test
-    test['age'] = imputer.transform(test[['age']])
-    
+def prep_telco_churn(df):
+    #cleaning and splitting our data
+    df = clean_telco_churn(df)
+    train, validate, test = telco_churn_split(df)
     return train, validate, test
+
+def get_metrics_binary(clf):
+    '''
+    get_metrics_binary takes in a confusion matrix (cnf) for a binary classifier and prints out metrics based on
+    values in variables named X_train, y_train, and y_pred.
+    
+    return: a classification report as a transposed DataFrame
+    '''
+    X_train, y_train = train[x_col], train[y_col]
+
+    X_validate, y_validate = validate[x_col], validate[y_col]
+
+    X_test, y_test = test[x_col], validate[y_col]
+    
+    accuracy = clf.score(X_train, y_train)
+    class_report = pd.DataFrame(classification_report(y_train, y_pred, output_dict=True)).T
+    conf = confusion_matrix(y_train, y_pred)
+    tpr = conf[1][1] / conf[1].sum()
+    fpr = conf[0][1] / conf[0].sum()
+    tnr = conf[0][0] / conf[0].sum()
+    fnr = conf[1][0] / conf[1].sum()
+    print(f'''
+    The accuracy for our model is {accuracy:.4}
+    The True Positive Rate is {tpr:.3}, The False Positive Rate is {fpr:.3},
+    The True Negative Rate is {tnr:.3}, and the False Negative Rate is {fnr:.3}
+    ''')
+    return class_report
+
 
